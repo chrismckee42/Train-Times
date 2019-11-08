04a39f27efb9fb87e83b0cc6530c725b41455ee2
Christopher McKee <chris.mckee.13@gmail.com>
2019-11-08 15:48:24 -0800

    adds firebase code to add new train times, no time calculations

--- javascript/train.js
+++ javascript/train.js
@@ -11,3 +11,54 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
+var database = firebase.database();
+
+
+// Initial Values
+var name = "";
+var dest = "";
+var time = "";
+var freq = 0;
+$("#add-user").on("click", function (event) {
+
+    event.preventDefault();
+
+    name = $("#name-input").val().trim();
+    dest = $("#dest-input").val().trim();
+    time = $("#time-input").val().trim();
+    freq = $("#freq-input").val().trim();
+
+    database.ref().push({
+        name,
+        dest,
+        time,
+        freq,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+});
+
+database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
+    console.log("Firebase")
+    console.log(snapshot.val());
+    console.log(snapshot.val().name);
+    console.log(snapshot.val().dest);
+    console.log(snapshot.val().time);
+    console.log(snapshot.val().freq);
+
+
+    var row = $("<tr>")
+    row.append($("<td>").text(snapshot.val().name))
+    row.append($("<td>").text(snapshot.val().dest))
+    row.append($("<td>").text(snapshot.val().time))
+    row.append($("<td>").text(5))
+    row.append($("<td>").text(snapshot.val().freq))
+
+    $("#table").append(row)
+
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
