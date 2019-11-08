e53336bd3d2efd92644f6312c6e449977dc881a0
Christopher McKee <chris.mckee.13@gmail.com>
2019-11-08 15:24:14 -0800

    adds html boilerplate and links to bootstrap, jquery and firebase

---
+++ javascript/train.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyArd55Fmm0T5pvCHYV-yoFhfkQ_51aqRNo",
+    authDomain: "train-times-8ab27.firebaseapp.com",
+    databaseURL: "https://train-times-8ab27.firebaseio.com",
+    projectId: "train-times-8ab27",
+    storageBucket: "train-times-8ab27.appspot.com",
+    messagingSenderId: "364832833754",
+    appId: "1:364832833754:web:65f083e89c34c8518965e3",
+    measurementId: "G-Z0QH80PRLP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
